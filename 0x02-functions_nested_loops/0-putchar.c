926bc6e6d770efd343c4bd7943891b138c8a10f6
bimbo7404 <bimconsult@yahoo.co.uk>
2023-08-18 01:49:03 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 */
 int main(void)
 {
-	char[10] = "_putchar";
+	char school[10] = "_putchar";
 
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(i);
+		_putchar(school[i]);
 	}
 	_putchar(10);
 	return (0);
